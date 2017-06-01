83dede775ecdb67a1fa93e69c019533c8c5f3aae
Olivier Cochard-Labbé <olivier@cochard.me>
2017-06-01 15:34:05 +0200

    Add configurations files

--- FreeBSD/testPR.sh
+++ FreeBSD/testPR.sh
@@ -31,6 +31,7 @@ echo "Downloading patch..."
 fetch -q -o ${PORTDIR}/${PATCH} "https://bz-attachments.freebsd.org/attachment.cgi?id=${ID}"
 grep -q 'DOCTYPE html' ${PORTDIR}/${PATCH} && die "Seems not a good patch (check ${PORTDIR}/${PATCH})"
 TOPATCH=$(grep -m 1 '\-\-\-' ${PORTDIR}/${PATCH} | cut -d ' ' -f 2)
+echo ${TOPATCH} | grep -q '/' || die "Patch didn't include full path"
 PORTNAME=$(echo $TOPATCH | cut -d '/' -f 1,2)
 [ -d ${PORTDIR}/${PORTNAME} ] || die "Can't found ${PORTDIR}/${PORTNAME}"
 # benchmarks/stress-ng/Makefile
@@ -41,6 +42,8 @@ ${SVNCMD} revert -q -R ${PORTDIR}/${PORTNAME}
 echo "Applying patch..."
 patch -s -C -E -p 0 -d ${PORTDIR} < ${PORTDIR}/${PATCH} || die "Can't apply patch correctly"
 patch -s -E -p 0 -d ${PORTDIR} < ${PORTDIR}/${PATCH}
+#echo "Portlint"
+#/usr/bin/which -s portlint && portlint -A ${PORTNAME}
 echo "poudriere testport -j 110amd64 -o ${PORTNAME}"
 poudriere testport -j 110amd64 -o ${PORTNAME}
 echo "poudriere testport -j 103i386 -o ${PORTNAME}"
