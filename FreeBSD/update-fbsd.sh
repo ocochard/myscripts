af132477bb24172a4c0a29d5d96f98bde71b2f5f
Olivier Cochard-Labbé <olivier@cochard.me>
2021-04-29 09:33:21 +0200

    adding new packages

--- FreeBSD/update-fbsd.sh
+++ FreeBSD/update-fbsd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Update FreeBSD and ports, then install new environment using ZFS BE
 set -eu
-curdir=$(pwd)
+script_dir=$(dirname $0)
 cd /usr/src
 git pull --ff-only
 make -j 32 buildworld buildkernel
@@ -9,6 +9,6 @@ poudriere ports -u
 # Warning: Rebuilding the jail will force a rebuild of all ports
 # But could be mandatory in case of video modules than need to be synced with kernel
 #poudriere jail -j builder -u -m src=/usr/src
-poudriere bulk -j builder -f ${curdir}/packages.list
+poudriere bulk -j builder -f ${script_dir}/packages.list
 tools/build/beinstall.sh
 echo "shutdown -r now"
