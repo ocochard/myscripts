1d20738660bacf1522f1a9649a058ded5d411e1f
Olivier Cochard-Labbé <olivier@cochard.me>
2021-06-01 19:56:30 +0200

    Adding new packages

--- FreeBSD/update-fbsd.sh
+++ FreeBSD/update-fbsd.sh
@@ -8,10 +8,14 @@ script_dir=$(dirname $script)
 cd /usr/src
 git pull --ff-only
 make -j 32 buildworld buildkernel
+ports_src=$(poudriere ports -lq | grep '^default' | awk {'print $5'})
+cd ${ports_src}
+git stash
 poudriere ports -u
+git stash pop
 # Warning: Rebuilding the jail will force a rebuild of all ports
 # But could be mandatory in case of video modules than need to be synced with kernel
 #poudriere jail -j builder -u -m src=/usr/src
 poudriere bulk -j builder -f ${script_dir}/packages.list
-env NO_PKG_UPGRADE=YES tools/build/beinstall.sh
+env NO_PKG_UPGRADE=YES /usr/src/tools/build/beinstall.sh
 echo "shutdown -r now"
