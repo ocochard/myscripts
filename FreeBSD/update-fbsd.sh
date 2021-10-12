342f029ea62cb27dfaecbe3bbe83992311b5a568
Olivier Cochard-Labbé <olivier@cochard.me>
2021-10-12 03:05:17 +0200

    Correct usage of src.conf src-env.conf and make.conf

--- FreeBSD/update-fbsd.sh
+++ FreeBSD/update-fbsd.sh
@@ -5,8 +5,50 @@ set -eu
 script=$(readlink -f $0)
 # Absolute path this script is in
 script_dir=$(dirname $script)
+echo "Setting up /etc/make.conf and kernel"
+if [ -f /etc/src.conf ]; then
+	mv /etc/src.conf /etc/src.conf.bak
+fi
+cat > /etc/src.conf <<EOF
+# Disable debugging assertions in LLVM
+WITHOUT_LLVM_ASSERTIONS=yes
+# Only build the required LLVM target support
+WITHOUT_LLVM_TARGET_ALL=yes
+# Disable assertions and statistics gathering in malloc(3)
+WITH_MALLOC_PRODUCTION=yes
+EOF
+
+if [ -f /etc/src-env.conf ]; then
+	mv  /etc/src-env.conf  /etc/src-env.conf.bak
+fi
+cat > /etc/src-env.conf <<EOF
+WITH_META_MODE=yes
+EOF
+
+cat > /usr/src/sys/amd64/conf/BBR <<EOF
+include GENERIC-NODEBUG
+ident           BBR
+options         TCPHPTS
+options         RATELIMIT
+makeoptions     WITH_EXTRA_TCP_STACKS=1
+options         KDB_UNATTENDED
+EOF
+
+if [ -f /etc/make.conf ]; then
+	mv /etc/make.conf /etc/make.conf.bak
+fi
+cat > /etc/make.conf <<EOF
+# Use our custom kernel configuration file
+KERNCONF=BBR
+EOF
+
+# Using META_MODE requiere filemon
+kldstat -qm filemon || kldload filemon
+
+echo "Updating source tree"
 cd /usr/src
 git pull --ff-only
+echo "Building world and kernel"
 make -j 32 buildworld buildkernel
 ports_src=$(poudriere ports -lq | grep '^default' | awk {'print $5'})
 cd ${ports_src}
@@ -22,7 +64,10 @@ if [ ! -f /usr/local/etc/poudriere.d/make.conf ]; then
 	echo "LICENSES_ACCEPTED+= Proprietary"
 	) > /usr/local/etc/poudriere.d/make.conf
 fi
-poudriere bulk -j builder -f ${script_dir}/packages.list
+echo "Building ports..."
+if ! poudriere bulk -j builder -f ${script_dir}/packages.list; then
+	echo "[WARNING] Some packages fails to build"
+fi
 cd /usr/src
 env NO_PKG_UPGRADE=YES /usr/src/tools/build/beinstall.sh
 echo "shutdown -r now"
