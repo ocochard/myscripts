4f6b05ac78cd2071472f2b9a0625cbc28565454c
Olivier Cochard-Labbé <olivier@cochard.me>
2022-09-23 15:01:48 +0200

    Fix ministat, add git check

--- FreeBSD/benches/buildworld.sh
+++ FreeBSD/benches/buildworld.sh
@@ -12,28 +12,35 @@ if [ $CPUS -le 4 ]; then
 else
 	JOBS=4
 fi
-RUNS=3
+
+RUNS=3						# Number of iteration for each bench
+RAMDISK="/usr/obj/ramdisk"	# RAM disk (tmpfs) directory
+SRCDIR="$RAMDISK/src"		# FreeBSD source directory
+
+# Function definitions
+
+# A usefull function (from: http://code.google.com/p/sh-die/)
+die() { echo -n "EXIT: " >&2; echo "$@" >&2; exit 1; }
 
 ### System check ###
+
+## Dependencies
+
+# git
+which -s git || die "git not available (used to download FreeBSD sources)"
 
 ## Space needed ##
 # - 1.2G for sources
 # - 10G for obj dir
 # - 1G free to compile
-# => 16G minimum to build from RAM
+# => 16G minimum to build from RAM, or more if ZFS in use?
+# ZFS cache could render useless the storage of sources in RAM
 
 PHYSMEM=$(sysctl -n hw.physmem)
 if [ $PHYSMEM -lt 17105900000 ]; then
-	echo "Need a minimum of 16G RAM"
-	exit 1
+	die "Need a minimum of 16G RAM"
+	# XXX Or switch to non-tmpfs
 fi
-
-## tooling ##
-
-# git
-
-RAMDISK="/usr/obj/ramdisk"
-SRCDIR="$RAMDISK/src"
 
 mkdir -p $RAMDISK
 
@@ -44,6 +51,7 @@ if mount | grep -q $RAMDISK; then
 	echo "Don't forget to unmount it next time!"
 else
 	# Build in a tmpfs, avoid benching hard disk i/o
+	# XXX How to avoid swap usage ?
 	mount -t tmpfs tmpfs $RAMDISK
 fi
 
@@ -54,7 +62,7 @@ git clone --depth 1 --branch main --single-branch https://git.freebsd.org/src.gi
 
 cd $SRCDIR
 
-echo "Result in $TMPDIR"
+echo "Results in $TMPDIR"
 
 # Init gnuplot data file
 for i in real user sys; do
@@ -67,7 +75,7 @@ while [ $JOBS -le $((CPUS * 2)) ]; do
 		# Forcing GENERIC kernel, no custom MAKE_CONF/SRCCONF/SRC_ENV_CONF
 		echo "Cleanup..."
 		env __MAKE_CONF=/dev/null SRC_ENV_CONF=/dev/null MAKEOBJDIRPREFIX=$RAMDISK \
-			make SRCCONF=/dev/null clean > /dev/null
+			make SRCCONF=/dev/null clean > /dev/null 2>&1
 		echo "Build..."
 		# Write log into ram disk to avoid benching local disk speed
 		env __MAKE_CONF=/dev/null SRC_ENV_CONF=/dev/null MAKEOBJDIRPREFIX=$RAMDISK \
@@ -80,25 +88,23 @@ while [ $JOBS -le $((CPUS * 2)) ]; do
 	#         0.00 real         0.00 user         0.00 sys
 	# real is 2, user is 4 and sys is 6
 	timepos=2
-	# ministat -qn output
-	# real is 3, user is 4, sys is 4
-	# x   3           112           134           124     123.33333     11.015141
-	minipos=3
 
 	for t in real user sys; do
 		cut -w -f $timepos $TMPDIR/buildbench.$JOBS.time > $TMPDIR/buildbench.$JOBS.$t
 		timepos=$((timepos + 2))
 
 		ministat -qn $TMPDIR/buildbench.$JOBS.$t > $TMPDIR/buildbench.$JOBS.$t.ministat
-		min=$(cut -w -f $minipos $TMPDIR/buildbench.$JOBS.$t.ministat)
-		max=$(cut -w -f $minipos $TMPDIR/buildbench.$JOBS.$t.ministat)
-		med=$(cut -w -f $minipos $TMPDIR/buildbench.$JOBS.$t.ministat)
+		# ministat output example:
+		#    N           Min           Max        Median           Avg        Stddev
+		#x   3       4230.49       4243.08        4231.5     4235.0233     6.9955295
+		# Min is 3, Max is 4, Median is 5
+		min=$(cut -w -f 3 $TMPDIR/buildbench.$JOBS.$t.ministat)
+		max=$(cut -w -f 4 $TMPDIR/buildbench.$JOBS.$t.ministat)
+		med=$(cut -w -f 5 $TMPDIR/buildbench.$JOBS.$t.ministat)
 		echo "$JOBS $med $min $max" >> $TMPDIR/gnuplot.$t.data
-		minipos=$((minipos + 1))
 	done # for t
 
 	# multiply job per 2
 	JOBS=$(( JOBS * 2 ))
 done # while JOBS
 umount $RAMDISK
-
